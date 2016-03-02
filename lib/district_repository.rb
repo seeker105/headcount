a37b6032299bb0b6465a24266835e66d2871e023
Christopher Soden <karavol27@gmail.com>
2016-03-02 01:30:43 -0700

    added highschool grad data to test data

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -17,15 +17,24 @@ class DistrictRepository
     e1 = Enrollment.new({:name => "ACADEMY 20",
       :kindergarten_participation => { 2010 => 0.3805,
         2011 => 0.35456,
-        2012 => 0.2687 }})
+        2012 => 0.2687 },
+      :high_school_graduation => { 2010 => 0.3434,
+        2011 => 0.6845,
+        2012 => 0.1123 }})
     e2 = Enrollment.new({:name => "GREEN MOUNTAIN",
       :kindergarten_participation => { 2010 => 0.5777,
         2011 => 0.8184,
-        2012 => 0.5678 }})
+        2012 => 0.5678 },
+      :high_school_graduation => { 2010 => 0.7709,
+        2011 => 0.657843845,
+        2012 => 0.659 }})
     e3 = Enrollment.new({:name => "COLORADO",
       :kindergarten_participation => { 2010 => 0.41897,
         2011 => 0.76984,
-        2012 => 0.3139 }})
+        2012 => 0.3139 },
+      :high_school_graduation => { 2010 => 0.1175,
+        2011 => 0.613,
+        2012 => 0.9358 }})
     enrollment_repo.enrollments = [e1, e2, e3]
     #-------------------
   end
