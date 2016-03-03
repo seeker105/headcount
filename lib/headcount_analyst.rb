44c2d05ea25cd6ba6c90baccef62ae2f97639f47
Matt Pindell <pindell.m@gmail.com>
2016-03-02 20:50:24 -0700

    method: kindergarten_participation_correlates_with_high_school_graduation

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -54,4 +54,19 @@ class HeadcountAnalyst
     (hs / state).round(3)
   end
 
+  def kindergarten_participation_correlates_with_high_school_graduation(for_hash)
+    if for_hash.has_value?('STATEWIDE')
+      var = district_repo.find_by_name('COLORADO').enrollment.graduation_avg_all_years
+      var > 0.7
+    elsif for_hash.has_key?(:for)
+      var = kindergarten_participation_against_high_school_graduation(for_hash[:for])
+      (0.6..1.5).member?(var)
+    elsif for_hash.has_key?(:across)
+      var = for_hash[:across].map do |district|
+        district_repo.find_by_name(district).enrollment.graduation_avg_all_years
+      end.reduce(:+) / for_hash[:across].count
+      var > 0.7
+    end
+  end
+
 end
