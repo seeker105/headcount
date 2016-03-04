b92bd53114c0a74bea3439f27e7400b3c2a26206
Matt Pindell <pindell.m@gmail.com>
2016-03-03 18:12:15 -0700

    working on kindergarten_participation_correlates_with_high_school_graduation

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -1,7 +1,9 @@
 require 'pry'
 require 'csv'
+require_relative '../lib/clean_data'
 
 class HeadcountAnalyst
+  include CleanData
   attr_reader :district_repo
 
   def initialize(district_repo)
@@ -11,11 +13,11 @@ class HeadcountAnalyst
   def kindergarten_participation_rate_variation(name, comparison)
     original = kd_participation_total_avg_for_location(name)
     compared = kd_participation_total_avg_for_location(comparison[:against])
-    (original / compared).round(3)
+    format_percentage(original / compared)
   end
 
   def kd_participation_total_avg_for_location(name)
-    district_repo.find_by_name(name).enrollment.kd_participation_avg_all_yrs
+    format_percentage(district_repo.find_by_name(name).enrollment.kd_participation_avg_all_yrs)
   end
 
   def kd_participation_avg_for_each_year(name)
@@ -23,50 +25,86 @@ class HeadcountAnalyst
   end
 
   def kindergarten_participation_rate_variation_trend(name, comparison)
+    # binding.pry
     original = kd_participation_avg_for_each_year(name)
     compared = kd_participation_avg_for_each_year(comparison[:against])
+    # binding.pry
     years_and_variation_trends(original, compared)
   end
 
   def years_and_variation_trends(original, compared)
     result = Hash.new
     original.each_key do |key|
-      result[key] = (original[key] / compared[key]).round(3)
+      result[key] = format_percentage(original[key] / compared[key])
     end
     result
-  end
-
-  def kindergarten_participation_against_high_school_graduation(name)
-    kindergarten_variation = kindergarten_participation_rate_divided_by_state_avg(name)
-    graduation_variation   = high_school_grad_rate_divided_by_state_avg(name)
-    (kindergarten_variation / graduation_variation).round(3)
   end
 
   def kindergarten_participation_rate_divided_by_state_avg(name)
     kg = district_repo.find_by_name(name).enrollment.kd_participation_avg_all_yrs
     state = district_repo.find_by_name('COLORADO').enrollment.kd_participation_avg_all_yrs
-    (kg / state).round(3)
+    format_percentage(kg / state)
   end
 
   def high_school_grad_rate_divided_by_state_avg(name)
     hs = district_repo.find_by_name(name).enrollment.graduation_avg_all_years
     state = district_repo.find_by_name('COLORADO').enrollment.graduation_avg_all_years
-    (hs / state).round(3)
+    format_percentage(hs / state)
+  end
+
+  def kindergarten_participation_against_high_school_graduation(name)
+    kindergarten_variation = kindergarten_participation_rate_divided_by_state_avg(name)
+    graduation_variation   = high_school_grad_rate_divided_by_state_avg(name)
+    format_percentage(kindergarten_variation / graduation_variation)
+  end
+
+  def compare_district_to_state_avg(name)
+    var = kindergarten_participation_against_high_school_graduation(name)
+    (0.6..1.5).member?(var)
   end
 
   def kindergarten_participation_correlates_with_high_school_graduation(for_hash)
-    if for_hash.has_value?('STATEWIDE')
-      var = district_repo.find_by_name('COLORADO').enrollment.graduation_avg_all_years
-      var > 0.7
-    elsif for_hash.has_key?(:for)
-      var = kindergarten_participation_against_high_school_graduation(for_hash[:for])
-      (0.6..1.5).member?(var)
-    elsif for_hash.has_key?(:across)
-      var = for_hash[:across].map do |district|
-        district_repo.find_by_name(district).enrollment.graduation_avg_all_years
-      end.reduce(:+) / for_hash[:across].count
-      var > 0.7
+    # if for_hash.has_value?('STATEWIDE')
+    #   all_districts = district_repo.districts.map do |district|
+    #     district.enrollment.graduation_avg_all_years
+    #   end
+    #   final = all_districts.select do |num|
+    #     (0.6...1.5).member?(num)
+    #   end
+    #   # binding.pry
+    #   if final.count > 127
+    #     return true
+    #   else
+    #     return false
+    #   end
+    #
+    # elsif for_hash.has_key?(:for)
+    #   compare_district_to_state_avg(for_hash[:for])
+    #
+    # elsif for_hash.has_key?(:across)
+    #   var = for_hash[:across].map do |district|
+    #     district_repo.find_by_name(district).enrollment.graduation_avg_all_years
+    #   end.reduce(:+) / for_hash[:across].count
+    #   var > 0.7
+    # end
+    unless for_hash.has_key?(:across)
+      name = for_hash[:for]
+      # binding.pry
+      puts "\n\n\n"
+      # binding.pry
+      puts kindergarten_participation_against_high_school_graduation("ACADEMY 20")
+      # binding.pry
+      # puts "Statewide ="
+      # binding.pry
+      binding.pry
+      result = []
+      district_repo.districts.each do |district|
+        name = district.name
+        calculated = kindergarten_participation_against_high_school_graduation(name)
+        result << calculated
+      end
     end
+
   end
 
 end
--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -10,62 +10,41 @@ require_relative '../lib/headcount_analyst'
 class HeadcountAnalystTest < Minitest::Test
 
   def setup
-    skip
-    district_1     = District.new({name: "ACADEMY 20"})
-    district_2     = District.new({name: "COLORADO"})
-    district_repo  = DistrictRepository.new([district_1, district_2])
-    enrollment_1   = Enrollment.new({name: "ACADEMY 20",
-                     :kindergarten_participation => { 2010 => 0.3915,
-                                                      2011 => 0.35356,
-                                                      2012 => 0.2677 }})
-    enrollment_2    = Enrollment.new({name: "COLORADO",
-                     :kindergarten_participation => { 2010 => 0.64019,
-                                                      2011 => 0.672,
-                                                      2012 => 0.695 }})
-    enrollment_repo = EnrollmentRepository.new([enrollment_1, enrollment_2])
-
-    # district_repo.load_enrollments(enrollment_repo)
-    # district_repo.enrollment_repo = enrollment_repo
-    # district_repo.load_enrollments
-
-    @h_analyst      = HeadcountAnalyst.new(district_repo)
-  end
-
-  def test_it_can_initialize_with_a_new_district_repository
-    skip
-    assert_kind_of HeadcountAnalyst, @h_analyst
-  end
-
-  def test_it_returns_the_kindergarten_participation_rate_variation
-    skip
-    colorado_truncated_avg = 0.669
-    academy_truncated_avg  = 0.337
-    truncated_rate_variation = 0.504
-    submitted = @h_analyst.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'COLORADO')
-
-    assert_equal truncated_rate_variation, submitted
-  end
-
-  def test_it_returns_the_kindergarten_participation_rate_trend
-    skip
-    submitted = @h_analyst.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO')
-    expected  = {2010=>0.611, 2011=>0.525, 2012=>0.384}
-    assert_equal expected, submitted
-  end
-
-  def test_it_returns_the_kindergarten_participation_rate_trend
     # skip
     district_repo  = DistrictRepository.new
     district_repo.load_data(:enrollment => {
       :kindergarten => "./data/Kindergartners in full-day program.csv",
       :high_school_graduation => "./data/High school graduation rates.csv"})
-    # district_repo.load_enrollments
 
-    # binding.pry
-    ha = HeadcountAnalyst.new(district_repo)
-    submitted = ha.kindergarten_participation_against_high_school_graduation('ACADEMY 20')
-    # expected  = 1.234
-    expected  = 0.641
+    @h_analyst      = HeadcountAnalyst.new(district_repo)
+  end
+
+  def test_it_can_initialize_with_a_new_district_repository
+    # skip
+    assert_kind_of HeadcountAnalyst, @h_analyst
+  end
+
+  def test_it_returns_the_kindergarten_participation_rate_variation_with_state_avg
+    # skip
+    submitted = @h_analyst.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'COLORADO')
+    expected  = 0.766
+
+    assert_equal expected, submitted
+  end
+
+  def test_it_returns_the_kindergarten_participation_rate_variation
+    # skip
+    submitted = @h_analyst.kindergarten_participation_rate_variation("GUNNISON WATERSHED RE1J", :against => "TELLURIDE R-1")
+    expected  = 1.126
+
+    assert_equal expected, submitted
+  end
+
+  def test_it_returns_the_kindergarten_participation_rate_variation_trend
+    skip
+    submitted = @h_analyst.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'YUMA SCHOOL DISTRICT 1')
+    expected  = {2010=>0.611, 2011=>0.525, 2012=>0.384}
+
     assert_equal expected, submitted
   end
 
