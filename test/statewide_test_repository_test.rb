b174961a8b76558c0b20528a7b588c88c1f91b74
Matt Pindell <pindell.m@gmail.com>
2016-03-05 16:18:51 -0700

    moved test to statewide_test_repo

--- test/statewide_test_repository_test.rb
+++ test/statewide_test_repository_test.rb
@@ -9,32 +9,25 @@ require_relative '../lib/statewide_test'
 
 class StatewideTestRepositoryTest < Minitest::Test
 
-  def setup
-    @stw_test_repo = StatewideTestRepository.new
-    # @stw_test_repo.load_data({ :statewide_testing => {
-    #   :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
-    #   :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
-    #   :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
-    #   :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
-    #   :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv" }
-    #   })
-    @stw_test_repo.load_data({ :statewide_testing => {
-      :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
-      :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
-      :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
-      :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
-      :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"}
-      })
-  end
+  @@stw_test_repo = StatewideTestRepository.new
+  @@stw_test_repo.load_data({ :statewide_testing => {
+    :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
+    :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
+    :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
+    :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
+    :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"}
+    })
 
   def test_can_create_statewide_test_repo
-    # skip
-    assert_kind_of StatewideTestRepository, @stw_test_repo
+    assert_kind_of StatewideTestRepository, @@stw_test_repo
   end
 
-  
+  def test_statewide_test_repo_finds_a_statewide_test_by_name
+    name      = 'ACADEMY 20'
+    submitted = @@stw_test_repo.find_by_name(name)
 
-
-
+    assert_kind_of StatewideTest, submitted
+    assert_equal name, submitted.name
+  end
 
 end
