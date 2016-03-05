92a1ad7a5445251981eae6b1e31aaeef0a685176
Matt Pindell <pindell.m@gmail.com>
2016-03-04 20:20:36 -0700

    refactored statewide_test

--- lib/statewide_test.rb
+++ lib/statewide_test.rb
@@ -23,10 +23,8 @@ class StatewideTest
   def proficient_by_grade(grade)
     raise UnknownDataError unless GRADE.member?(grade)
     case grade
-    when 3
-      @third_grade
-    when 8
-      @eighth_grade
+    when 3 then @third_grade
+    when 8 then @eighth_grade
     end
   end
 
@@ -39,42 +37,5 @@ class StatewideTest
     raise UnknownDataError unless GRADE.member?(grade)
 
   end
-
-  # def kindergarten_participation_by_year
-  #   kindergarten_participation
-  # end
-  #
-  # def kindergarten_participation_in_year(year)
-  #   if contains_year?(year, kindergarten_participation)
-  #     kindergarten_participation.fetch(year)
-  #   end
-  # end
-  #
-  # def contains_year?(year, group)
-  #   raise ArgumentError unless year.is_a? Fixnum
-  #   group.has_key?(year)
-  # end
-  #
-  # def kd_participation_avg_all_yrs
-  #   compute_avg(kindergarten_participation.values)
-  # end
-  #
-  # def graduation_rate_by_year
-  #   high_school_graduation
-  # end
-  #
-  # def graduation_avg_all_years
-  #   compute_avg(high_school_graduation.values)
-  # end
-  #
-  # def compute_avg(array)
-  #   array.reduce(:+) / array.count
-  # end
-  #
-  # def graduation_rate_in_year(year)
-  #   if contains_year?(year, high_school_graduation)
-  #     high_school_graduation.fetch(year)
-  #   end
-  # end
 
 end
